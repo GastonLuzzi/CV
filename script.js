3963e78616fbfae12221ed9203421e58e383ce90
Gastón <gastonluzzi@gmail.com>
2023-09-15 18:07:00 -0300

    commit 1

---
+++ script.js
@@ -0,0 +1,8 @@
+var botonOcultar = document.getElementById("ocultarEducacion");
+var parrafoEducacion = document.getElementById("educacion");
+
+
+botonOcultar.addEventListener("click", function() {
+
+    parrafoEducacion.style.display = "none";
+});
